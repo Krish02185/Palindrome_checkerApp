049744a4fca3f196dca529c26e88f200c1396330
Krish02185 <krishna006shukla@gmail.com>
2026-02-24 14:29:16 +0530

    Palindrome_checkerApp

--- src/Palindrome_checkerApp.java
+++ src/Palindrome_checkerApp.java
@@ -1,31 +1,8 @@
-import java.util.LinkedList;
-import java.util.Stack;
-import  java.util.Queue;
-
 public class Palindrome_checkerApp {
-    public static void main (String[] args) {
-        String input = "civic";
 
-        Queue<Character> queue = new LinkedList<>();
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            queue.add(c);
-            stack.push(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (!queue.isEmpty()) {
-            if (!queue.poll().equals(stack.pop())) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("Input text : " + input);
-
-        System.out.println("Is Palindrome? : " + isPalindrome);
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
